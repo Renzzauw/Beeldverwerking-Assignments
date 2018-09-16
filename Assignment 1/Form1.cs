4def6b03661aecd96f39c4c19dbb2bd42b1e32b4
Renzzauw <renzo@schindeler.nl>
2018-09-16 14:20:05 +0200

    Linear filter gefixt + gaussian blur

--- Assignment 1/Form1.cs
+++ Assignment 1/Form1.cs
@@ -10,33 +10,33 @@ using System.IO;
 
 namespace INFOIBV
 {
-    public partial class INFOIBV : Form
-    {
-        private Bitmap InputImage;
-        private Bitmap OutputImage;
+	public partial class INFOIBV : Form
+	{
+		private Bitmap InputImage;
+		private Bitmap OutputImage;
 
-        public INFOIBV()
-        {
-            InitializeComponent();
-        }
+		public INFOIBV()
+		{
+			InitializeComponent();
+		}
 
-        private void LoadImageButton_Click(object sender, EventArgs e)
-        {
-           if (openImageDialog.ShowDialog() == DialogResult.OK)             // Open File Dialog
-            {
-                string file = openImageDialog.FileName;                     // Get the file name
-                imageFileName.Text = file;                                  // Show file name
-                if (InputImage != null) InputImage.Dispose();               // Reset image
-                InputImage = new Bitmap(file);                              // Create new Bitmap from file
-                if (InputImage.Size.Height <= 0 || InputImage.Size.Width <= 0 ||
-                    InputImage.Size.Height > 512 || InputImage.Size.Width > 512) // Dimension check
-                    MessageBox.Show("Error in image dimensions (have to be > 0 and <= 512)");
-                else
-                    pictureBox1.Image = (Image) InputImage;                 // Display input image
-            }
-        }
+		private void LoadImageButton_Click(object sender, EventArgs e)
+		{
+			if (openImageDialog.ShowDialog() == DialogResult.OK)             // Open File Dialog
+			{
+				string file = openImageDialog.FileName;                     // Get the file name
+				imageFileName.Text = file;                                  // Show file name
+				if (InputImage != null) InputImage.Dispose();               // Reset image
+				InputImage = new Bitmap(file);                              // Create new Bitmap from file
+				if (InputImage.Size.Height <= 0 || InputImage.Size.Width <= 0 ||
+					InputImage.Size.Height > 512 || InputImage.Size.Width > 512) // Dimension check
+					MessageBox.Show("Error in image dimensions (have to be > 0 and <= 512)");
+				else
+					pictureBox1.Image = (Image)InputImage;                 // Display input image
+			}
+		}
 
-        private void applyButton_Click(object sender, EventArgs e)
+		private void applyButton_Click(object sender, EventArgs e)
 		{
 			if (InputImage == null) return;                                 // Get out if no input image
 			if (OutputImage != null) OutputImage.Dispose();                 // Reset output image
@@ -67,19 +67,18 @@ namespace INFOIBV
 			CalculateAlowAndAhigh(Image, ref aLow, ref aHigh);              // Calculate a-low and a-high 
 
 			// (1) Grayscale Conversion
-			GrayscaleConversion(Image);
+			//GrayscaleConversion(Image);
 
 			// (2) Contrast Adjustment
-			ContrastAdjustment(Image, aLow, aHigh);
+			//ContrastAdjustment(Image, aLow, aHigh);
 
 			// (3) Gaussian Filter
-			// TODO: Fix kernel creation, values do not add up to 1
-			int kernelSize = 3;												// Size of the kernel
-			float sigma = 1f;												// Sigma
+			int kernelSize = 3;                                             // Size of the kernel
+			float sigma = 1f;                                               // Sigma
 			double[,] kernel = GaussianFilter(kernelSize, sigma);
 
 			// (4) Linear Filtering
-			// TODO: Test if this works when the kernel is fixed
+			// TODO: sneller maken? Hij doet het, maar bij hogere kernelgrootte word ie heel sloom
 			LinearFiltering(Image, kernel);
 
 			// (5) Nonlinear Filtering
@@ -90,7 +89,7 @@ namespace INFOIBV
 
 			// (7) Thresholding
 			int threshold = 130;
-			Thresholding(Image, threshold);
+			//Thresholding(Image, threshold);
 
 			//==========================================================================================
 
@@ -120,7 +119,7 @@ namespace INFOIBV
 					progressBar.PerformStep();                                                      // Increment progress bar
 				}
 			}
-			progressBar.Visible = false;															// Hide progress bar
+			progressBar.Visible = false;                                                            // Hide progress bar
 		}
 
 		// (2) Contrast Adjustment
@@ -151,23 +150,35 @@ namespace INFOIBV
 			double constant = 1d / (2 * Math.PI * sigma * sigma);           // Precalculate some stuff to prevent repeating calculations
 
 			double sum = 0;                                                 // Keep track of the total of all the kernel values
-			for (int x = -center; x <= center; x++)
+			for (int x = 0; x < kernelSize; x++)
 			{
-				for (int y = -center; y <= center; y++)
+				for (int y = 0; y < kernelSize; y++)
 				{
 					int xDist = x - center;
 					int yDist = y - center;
-					gaussianKernel[x + center, y + center] = constant * Math.Exp(-(xDist * xDist + yDist * yDist) / (2 * sigma * sigma));
-					sum += gaussianKernel[x + center, y + center];
-					progressBar.PerformStep();								// Increment progress bar	
+					gaussianKernel[x, y] = /*constant **/ Math.Exp(-(xDist * xDist + yDist * yDist) / (2 * sigma * sigma));
+					sum += gaussianKernel[x, y];
+					progressBar.PerformStep();                              // Increment progress bar	
 				}
 			}
+
+			double factor = 1 / sum;                                        // Create a factor to multiple with each kernel field
+			double newSum = 0;
+			for (int x = 0; x < kernelSize; x++)
+			{
+				for (int y = 0; y < kernelSize; y++)
+				{
+					gaussianKernel[x, y] = factor * gaussianKernel[x, y];   // Normalize the kernel so all values add up to 1
+					newSum += gaussianKernel[x, y];                         // Add up all values to check if the sum is equal to 1
+				}
+			}
+
 			progressBar.Visible = false;                                    // Hide progress bar
 
 			// *** Just some printing to the console to check the values of the kernel *** //
 
 			// Write the sum of all the kernel values to the console
-			Console.WriteLine("Kernel values sum: " + sum);
+			Console.WriteLine("Kernel values sum: " + newSum);
 
 			// Display kernel values in console as a matrix
 			Console.WriteLine("Kernel values matrix:");
@@ -195,22 +206,22 @@ namespace INFOIBV
 				for (int y = edgeDistance; y < InputImage.Size.Height - edgeDistance - 1; y++)
 				{
 					double newColor = 0;
-					Color pixelColor = Image[x, y];																// Get the pixel color at coordinate (x,y)
+					Color pixelColor = Image[x, y];                                                             // Get the pixel color at coordinate (x,y)
 
 					for (int u = -edgeDistance; u < edgeDistance; u++)
 					{
 						for (int v = -edgeDistance; v < edgeDistance; v++)
 						{
 							newColor += kernel[u + edgeDistance, v + edgeDistance] * Image[x + u, y + v].R;     // Calculate the new color value with the kernel
-							progressBar.PerformStep();															// Increment progress bar	
+							progressBar.PerformStep();                                                          // Increment progress bar	
 						}
 					}
 
-					Color updatedColor = Color.FromArgb((int)newColor, (int)newColor, (int)newColor);			// Grab only the red value on that pixel
-					Image[x, y] = updatedColor;																	// Set the new pixel color at coordinate (x,y)
+					Color updatedColor = Color.FromArgb((int)newColor, (int)newColor, (int)newColor);           // Grab only the red value on that pixel
+					Image[x, y] = updatedColor;                                                                 // Set the new pixel color at coordinate (x,y)
 				}
 			}
-			progressBar.Visible = false;																		// Hide progress bar
+			progressBar.Visible = false;                                                                        // Hide progress bar
 		}
 
 		// (7) Thresholding
@@ -233,7 +244,7 @@ namespace INFOIBV
 				}
 			}
 			progressBar.Visible = false;                                                        // Hide progress bar
-		}		
+		}
 
 		private void CalculateAlowAndAhigh(Color[,] Image, ref Color aLow, ref Color aHigh)
 		{
@@ -251,14 +262,14 @@ namespace INFOIBV
 				}
 			}
 			progressBar.Visible = false;                                                        // Hide progress bar
-		}		
+		}
 
 		private void saveButton_Click(object sender, EventArgs e)
-        {
-            if (OutputImage == null) return;                                // Get out if no output image
-            if (saveImageDialog.ShowDialog() == DialogResult.OK)
-                OutputImage.Save(saveImageDialog.FileName);                 // Save the output image
-        }
+		{
+			if (OutputImage == null) return;                                // Get out if no output image
+			if (saveImageDialog.ShowDialog() == DialogResult.OK)
+				OutputImage.Save(saveImageDialog.FileName);                 // Save the output image
+		}
 
 		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
 		{
